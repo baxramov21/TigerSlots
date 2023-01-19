c4cd8daa00675548b52a234aafaa872a1713cb10
Sheikh <barsamaniya11@gmail.com>
2023-01-19 21:38:33 +0500

    Added data binding

--- app/src/main/java/com/sheikh/tigerslots/presentation/ui/GameFragment.kt
+++ app/src/main/java/com/sheikh/tigerslots/presentation/ui/GameFragment.kt
@@ -5,15 +5,43 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import com.sheikh.tigerslots.R
+import com.sheikh.tigerslots.databinding.FragmentGameBinding
+import com.sheikh.tigerslots.presentation.ui.viewmodel.GameViewModel
+import com.sheikh.tigerslots.presentation.ui.viewmodel.ViewModelFactory
 
 class GameFragment : Fragment() {
+
+    private var _binding: FragmentGameBinding? = null
+    private val binding: FragmentGameBinding
+        get() = _binding ?: throw NullPointerException("FragmentGameBinding is null")
+
+    private val viewModelFactory by lazy {
+        ViewModelFactory(requireActivity().application)
+    }
+
+    private val viewModel by lazy {
+        ViewModelProvider(this, viewModelFactory)[GameViewModel::class.java]
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.fragment_game, container, false)
+        _binding = FragmentGameBinding.inflate(LayoutInflater.from(requireContext()))
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        if (_binding != null) {
+            _binding = null
+        }
+        super.onDestroyView()
     }
 
     companion object {
---
+++ app/src/main/java/com/sheikh/tigerslots/presentation/ui/binding/GameBindingAdapter.kt
@@ -0,0 +1,29 @@
+package com.sheikh.tigerslots.presentation.ui.binding
+
+import android.content.res.Resources
+import android.widget.TextView
+import androidx.databinding.BindingAdapter
+import com.sheikh.tigerslots.R
+
+@BindingAdapter("showDeposit")
+fun bindDeposit(textView: TextView, deposit: Int) {
+    val depositText = String.format(getResources(textView).getString(R.string.cash), deposit)
+    textView.text = depositText
+}
+
+@BindingAdapter("showBetAmount")
+fun bindBetAmount(textView: TextView, betAmount: Int) {
+    val betAmountText =
+        String.format(getResources(textView).getString(R.string.bet_amount), betAmount)
+    textView.text = betAmountText
+}
+
+@BindingAdapter("showWinAmount")
+fun bindWinAmount(textView: TextView, betAmount: Int) {
+    val winAmountText =
+        String.format(getResources(textView).getString(R.string.win_amount), betAmount)
+    textView.text = winAmountText
+}
+
+
+private fun getResources(textView: TextView): Resources = textView.context.resources
